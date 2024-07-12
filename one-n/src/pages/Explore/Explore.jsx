23eceee3adb22c52c36f5748a1ac876f87c49c5e
uthem150 <uthem150@gmail.com>
2024-07-13 01:49:49 +0900

    Style. 포인터 지정 및 주석 추가

--- one-n/src/pages/Explore/Explore.jsx
+++ one-n/src/pages/Explore/Explore.jsx
@@ -9,44 +9,51 @@ const Explore = () => {
   const baseUrl = "https://n1.junyeong.dev/api";
   const navigate = useNavigate();
 
-  const [data, setData] = useState([]);
-  const [searchKeyword, setSearchKeyword] = useState("");
-  const [signinData, setSigninData] = useState(null);
+  const [data, setData] = useState([]); //검색 결과 데이터 저장
+  const [searchKeyword, setSearchKeyword] = useState(""); //입력한 검색어 저장
+  const [signinData, setSigninData] = useState(null); //로그인 데이터 저장
   const [searchHistory, setSearchHistory] = useState([]); //로컬 스토리지에 저장된 목록
   const [searchClicked, setSearchClicked] = useState(false); //검색버튼이 눌렸는지 확인
 
   useEffect(() => {
+    //sessionStorage에 저장된 로그인 데이터
     const storedSigninData = sessionStorage.getItem("signinData");
     if (storedSigninData) {
       setSigninData(JSON.parse(storedSigninData));
     }
 
+    //localStorage에 저장된 검색 기록
     const storedSearchHistory = localStorage.getItem("searchHistory");
     if (storedSearchHistory) {
       setSearchHistory(JSON.parse(storedSearchHistory));
     }
   }, []);
 
+  //사진 클릭하면 /recipe/{id} 경로로 이동
   const handlePhotoClick = (id) => {
     navigate(`/recipe/${id}`);
   };
 
+  // 검색입력 변경될 때 마다 업데이트
   const handleSearchInputChange = (e) => {
     setSearchKeyword(e.target.value);
   };
 
   const handleSearch = () => {
+    // 빈 문자열 검색일때 종료
     if (searchKeyword.trim() === "") return;
 
+    //중복된 키워드 제거하고, 새로운 검색어를 맨 앞에 추가
     const updatedSearchHistory = [
       searchKeyword,
       ...searchHistory.filter((keyword) => keyword !== searchKeyword),
     ];
     setSearchHistory(updatedSearchHistory);
+    //localStorage에 검색 기록 저장
     localStorage.setItem("searchHistory", JSON.stringify(updatedSearchHistory));
 
     fetchRecipes(searchKeyword);
-    setSearchClicked(!searchClicked); //상태 토글
+    setSearchClicked(true);
   };
 
   const fetchRecipes = (searchKeyword) => {
@@ -59,7 +66,7 @@ const Explore = () => {
           ...item,
           thumbnail_image: `${baseUrl}/${item.thumbnail_image}`,
         }));
-        setData(updatedData);
+        setData(updatedData); //받아온 데이터 저장
         console.log(updatedData);
       })
       .catch((error) => {
@@ -68,6 +75,7 @@ const Explore = () => {
   };
 
   const handleDeleteHistory = (keyword) => {
+    //해당 keyword지운 배열 생성
     const updatedSearchHistory = searchHistory.filter(
       (item) => item !== keyword
     );
@@ -76,6 +84,7 @@ const Explore = () => {
   };
 
   useEffect(() => {
+    //빈 문자열 아니면, fetch
     if (searchKeyword.trim() !== "") {
       fetchRecipes(searchKeyword);
     }
@@ -91,10 +100,15 @@ const Explore = () => {
           placeholder="검색어를 입력하세요"
           className="search"
         />
-        <img src={search} onClick={handleSearch} alt="Search" />
+        <img
+          className="search-btn"
+          src={search}
+          onClick={handleSearch}
+          alt="Search"
+        />
       </div>
 
-      {searchHistory && searchHistory.length > 0 && (
+      {!searchClicked && searchHistory && searchHistory.length > 0 && (
         <div>
           <div style={{ fontSize: "15px", margin: "15px 15px" }}>
             <strong>최근 검색어</strong>
